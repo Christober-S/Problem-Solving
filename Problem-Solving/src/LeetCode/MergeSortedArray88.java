67ce513f2801cd272f1713b0606e77eee34e2ffe
Christober <christobersg@gmail.com>
2024-06-08 06:22:17 +0530

    leetcode

---
+++ Problem-Solving/src/LeetCode/MergeSortedArray88.java
@@ -0,0 +1,30 @@
+package LeetCode;
+
+public class MergeSortedArray88 {
+    class Solution {
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+            int last = m + n - 1;
+
+            // Merge in reverse order
+            while (n > 0 && m > 0){
+                if (nums1[m-1] > nums2[n-1]){
+                    nums1[last] = nums1[m-1];
+                    m--;
+                }
+                else{
+                    nums1[last] = nums2[n-1];
+                    n--;
+                }
+                last--;
+            }
+
+            // Fill nums1 with leftover nums2 elements
+            while(n > 0){
+                nums1[last] = nums2[n-1];
+                n--;
+                last--;
+            }
+        }
+    }
+
+}
